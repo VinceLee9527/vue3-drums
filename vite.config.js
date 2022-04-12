0cd2a0063d3ca41635eb19341db4a3ef50a28d11
Vince <vlee9527@gmail.com>
2022-04-13 00:57:30 +0800

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vue3-drums/",
   plugins: [vue()],
-  build: {
-    minify: true,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  base: process.env.NODE_ENV === "production" ? "/vue3-drums/" : "/",
 });
